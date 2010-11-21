64e500d2223d0579a5f18064611380ffce9dc62d
Shoji Ihara <iharashoji@ihara-shouer-no-MacBook.local>
2010-11-22 01:16:20 +0900

    Added connection check routine

--- src/com/pitecan/gyaco/GyacoService.java
+++ src/com/pitecan/gyaco/GyacoService.java
@@ -8,6 +8,9 @@ import java.util.*;
 import android.app.*;
 import android.appwidget.AppWidgetManager;
 import android.content.*;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -17,108 +20,101 @@ import android.media.MediaPlayer.OnCompletionListener;
 
 import android.os.Environment;
 
-     
+
 //ホームウィジェットを制御するサービス
 public class GyacoService extends Service {
-    private static final String ACTION_BTNCLICK =
-        "com.pitecan.GyacoService.ACTION_BTNCLICK";
+	private static final String ACTION_BTNCLICK =
+		"com.pitecan.GyacoService.ACTION_BTNCLICK";
 
-    private static MediaPlayer mp;
-    private Timer timer;
+	private static MediaPlayer mp;
 
-    //サービス開始時に呼ばれる
-    @Override
-    public void onStart(Intent intent,int startId) {
-        super.onStart(intent, startId);
-        
-	// mp = MediaPlayer.create(this, R.raw.cabbage); リソース利用の場合
-	this.download();
+	//サービス開始時に呼ばれる
+	@Override
+	public void onStart(Intent intent,int startId) {
+		super.onStart(intent, startId);
 
-        //リモートビューの取得
-        AppWidgetManager manager=AppWidgetManager.getInstance(this);
-        RemoteViews view=new RemoteViews(getPackageName(),R.layout.gyaco);
-        if (ACTION_BTNCLICK.equals(intent.getAction())) {
-            btnClicked(view);
-        }
-         
-        //button1とボタンクリックイベントの関連付け
-        Intent newintent=new Intent();
-        newintent.setAction(ACTION_BTNCLICK);
-        PendingIntent pending=PendingIntent.getService(this,0,newintent,0);
-        view.setOnClickPendingIntent(R.id.button1,pending);
-        
-        //ホームウィジェットの更新
-        ComponentName widget=new ComponentName(this,Gyaco.class);
-        manager.updateAppWidget(widget,view);
-    }
+		//リモートビューの取得
+		AppWidgetManager manager=AppWidgetManager.getInstance(this);
+		RemoteViews view=new RemoteViews(getPackageName(),R.layout.gyaco);
+		if (ACTION_BTNCLICK.equals(intent.getAction())) {
+			btnClicked(view);
+		}
 
-    //バインダーを返す
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-     
-    //ボタンクリック時に呼ばれる
-    public void btnClicked(RemoteViews view){
-	//mp.start(); // 音声再生
-	playSound();
-    }
+		//button1とボタンクリックイベントの関連付け
+		Intent newintent=new Intent();
+		newintent.setAction(ACTION_BTNCLICK);
+		PendingIntent pending=PendingIntent.getService(this,0,newintent,0);
+		view.setOnClickPendingIntent(R.id.button1,pending);
 
-    private void playSound(){
-	FileInputStream fs = null;
-	MediaPlayer mp = new MediaPlayer();
-	mp.setOnCompletionListener(new OnCompletionListener(){
-		@Override
-		    public void onCompletion(MediaPlayer mp) {
-		}});
-	try {
-	    // http://sites.google.com/site/tuyano/java/android/07
-	    // fs = new FileInputStream("/data/data/" + this.getPackageName() + "/files/" + Consts.FILENAME);
-	    fs = openFileInput(Consts.FILENAME); // /data/data... と同じことなのだと思う
-	    if(fs != null){
-		mp.setDataSource(fs.getFD());
-		mp.prepare();
-		mp.start();
-	    }
-	} catch(Exception e) {
-	    e.printStackTrace();
-	}	
-    }
-
-    private void downloadSoundData(){
-	//notification();
-	download();
-	if(checkHash()){
-	    //
+		//ホームウィジェットの更新
+		ComponentName widget=new ComponentName(this,Gyaco.class);
+		manager.updateAppWidget(widget,view);
 	}
-    }
 
-    private void download() {
-	if (timer != null) {
-	    timer.cancel();
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
 	}
-	timer = new Timer();
-	TimerTask timerTask = new TimerTask() {			
-		public void run() {
-		    Downloader d = new Downloader();
-		    d.setConnectTimeout(Consts.CONNECT_TIMEOUT); 
-		    d.setReadTimeout(Consts.READ_TIMEOUT); 
-		    byte[] b;
-		    try {
+
+	public void btnClicked(RemoteViews view){
+		downloadAndPlay();
+	}
+
+	private void playSound(){
+		FileInputStream fs = null;
+		MediaPlayer mp = new MediaPlayer();
+		mp.setOnCompletionListener(new OnCompletionListener(){
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+			}});
+		try {
+			fs = openFileInput(Consts.FILENAME);
+			if(fs != null){
+				mp.setDataSource(fs.getFD());
+				mp.prepare();
+				mp.start();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}	
+	}
+
+	private void downloadAndPlay(){
+		//notification();
+		if(checkConnectionStatus()){
+			if(checkHash()){
+				download();
+			}
+		}
+		playSound();
+	}
+
+	private void download() {
+		Downloader d = new Downloader();
+		d.setConnectTimeout(Consts.CONNECT_TIMEOUT); 
+		d.setReadTimeout(Consts.READ_TIMEOUT); 
+		byte[] b;
+		try {
 			b = d.getContent(Consts.DOWNLOAD_URL);
 			Log.v("Gyaco", "Download finished");
 			FileOutputStream fos = openFileOutput(Consts.FILENAME, MODE_PRIVATE);
 			fos.write(b);
 			fos.close();
-		    } catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		    }
 		}
-	    };
-	timer.schedule(timerTask, Consts.DOWNLOAD_DELAY);
-    }
+	}
 
-    private boolean checkHash(){
-	return true;
-    }
+	private boolean checkConnectionStatus(){
+		ConnectivityManager conn_manager = (ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo netinfo = conn_manager.getActiveNetworkInfo();
+		if(netinfo != null && netinfo.isConnected()){
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkHash(){
+		return true;
+	}
 }
